3345fe6c2bd61d28699cce2061912d528e7ddf89
morganawhitelaw-cloud <morganawhitelaw@gmail.com>
2026-02-12 19:50:08 -0800

    static upload

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: isProd ? "/godmap" : "",
+  assetPrefix: isProd ? "/godmap/" : "",
   reactCompiler: true,
   allowedDevOrigins: ["127.0.0.1", "localhost"],
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
